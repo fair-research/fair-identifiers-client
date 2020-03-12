9f121bc9bf4af324aac441f90493636280af2138
Nickolaus D. Saint <nick@globus.org>
2020-03-12 14:46:43 -0700

    Added content type to setup.py to fix markdown render, Thanks Mike!

--- setup.py
+++ setup.py
@@ -9,11 +9,12 @@ with open(os.path.join(BASE_DIR, 'README.md')) as f:
 
 setup(
     name='fair-identifiers-client',
-    version='0.3.0.dev3',
+    version='0.3.0.dev4',
     url='https://github.com/fair-research/fair-identifiers-client',
     author="FAIR Research Team",
     description='FAIR Research Identifiers Service Client',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     packages=find_packages(),
     install_requires=[
